da7d5729ecb5f7a5f48ef700e74896c17289a507
FREDOM1979 <5baxoff@gmail.com>
2022-07-03 11:44:15 +0600

    онбнгу

--- plugins/random-wallpaper-aesthetic.js
+++
@@ -1,8 +0,0 @@
-let fetch = require('node-fetch')
-     let handler  = async (m, { conn, usedPrefix, command }) => {
-    heum = await fetch(`https://api-reysekha.herokuapp.com/api/wallpaper/wallhp?apikey=APIKEY`)
-    json = await heum.buffer()
-   conn.sendButtonImg(m.chat, json, '✨ *Aquí está tú fondo aesthetic!!*', 'Gata Dios', '🔄 SIGUIENTE', `${usedPrefix + command}`, m, false)
-}
-handler.command = /^(wpaesthetic|fondoaesthetic|wallpaperaesthetic|aestheticfondo|aestheticwallpaper)$/i
-module.exports = handler
